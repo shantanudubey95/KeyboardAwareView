e1c688c49428477a57e9fa0c730946e2274e57d0
shantanudubey95 <109502475+shantanudubey95@users.noreply.github.com>
2022-10-25 11:55:37 +0530

    Working keyboard aware view

--- App.tsx
+++ App.tsx
@@ -1,12 +1,79 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet, Text, TextInput, ScrollView } from 'react-native';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <KeyboardAwareScrollView>
+      <ScrollView contentContainerStyle={styles.container}>
+        <StatusBar style="auto" />
+        <Text>Open up App.tsx to start working on your app!</Text>
+        <Text>First input box</Text>
+        <TextInput style={{ width: '90%', height: 30, borderWidth: 2 }} />
+        <Text>Second input box</Text>
+        <TextInput style={{ width: '90%', height: 30, borderWidth: 2 }} />
+        <Text>Third input box</Text>
+        <TextInput style={{ width: '90%', height: 30, borderWidth: 2 }} />
+        <Text>Fourth input box</Text>
+        <TextInput style={{ width: '90%', height: 30, borderWidth: 2 }} />
+        <Text>Fifth input box</Text>
+        <TextInput style={{ width: '90%', height: 30, borderWidth: 2 }} />
+        <Text>Fifth input box</Text>
+        <TextInput style={{ width: '90%', height: 30, borderWidth: 2 }} />
+        <Text>Fifth input box</Text>
+        <TextInput style={{ width: '90%', height: 30, borderWidth: 2 }} />
+        <Text>Fifth input box</Text>
+        <TextInput style={{ width: '90%', height: 30, borderWidth: 2 }} />
+        <Text>Fifth input box</Text>
+        <TextInput style={{ width: '90%', height: 30, borderWidth: 2 }} />
+        <Text>Fifth input box</Text>
+        <TextInput style={{ width: '90%', height: 30, borderWidth: 2 }} />
+        <Text>Fifth input box</Text>
+        <TextInput style={{ width: '90%', height: 30, borderWidth: 2 }} />
+        <Text>Fifth input box</Text>
+        <TextInput style={{ width: '90%', height: 30, borderWidth: 2 }} />
+        <Text>Fifth input box</Text>
+        <TextInput style={{ width: '90%', height: 30, borderWidth: 2 }} />
+        <Text>Fifth input box</Text>
+        <TextInput style={{ width: '90%', height: 30, borderWidth: 2 }} />
+        <Text>Fifth input box</Text>
+        <TextInput style={{ width: '90%', height: 30, borderWidth: 2 }} />
+        <Text>Fifth input box</Text>
+        <TextInput style={{ width: '90%', height: 30, borderWidth: 2 }} />
+        <Text>Fifth input box</Text>
+        <TextInput style={{ width: '90%', height: 30, borderWidth: 2 }} />
+        <Text>Fifth input box</Text>
+        <TextInput style={{ width: '90%', height: 30, borderWidth: 2 }} />
+        <Text>Fifth input box</Text>
+        <TextInput style={{ width: '90%', height: 30, borderWidth: 2 }} />
+        <Text>Fifth input box</Text>
+        <TextInput style={{ width: '90%', height: 30, borderWidth: 2 }} />
+        <Text>Fifth input box</Text>
+        <TextInput style={{ width: '90%', height: 30, borderWidth: 2 }} />
+        <Text>Fifth input box</Text>
+        <TextInput style={{ width: '90%', height: 30, borderWidth: 2 }} />
+        <Text>Fifth input box</Text>
+        <TextInput style={{ width: '90%', height: 30, borderWidth: 2 }} />
+        <Text>Fifth input box</Text>
+        <TextInput style={{ width: '90%', height: 30, borderWidth: 2 }} />
+        <Text>Fifth input box</Text>
+        <TextInput style={{ width: '90%', height: 30, borderWidth: 2 }} />
+        <Text>Fifth input box</Text>
+        <TextInput style={{ width: '90%', height: 30, borderWidth: 2 }} />
+        <Text>Fifth input box</Text>
+        <TextInput style={{ width: '90%', height: 30, borderWidth: 2 }} />
+        <Text>Fifth input box</Text>
+        <TextInput style={{ width: '90%', height: 30, borderWidth: 2 }} />
+        <Text>Fifth input box</Text>
+        <TextInput style={{ width: '90%', height: 30, borderWidth: 2 }} />
+        <Text>Fifth input box</Text>
+        <TextInput style={{ width: '90%', height: 30, borderWidth: 2 }} />
+        <Text>Fifth input box</Text>
+        <TextInput style={{ width: '90%', height: 30, borderWidth: 2 }} />
+        <Text>Fifth input box</Text>
+        <TextInput style={{ width: '90%', height: 30, borderWidth: 2 }} />
+        <Text>Open up App.tsx to start working on your app!</Text>
+      </ScrollView>
+    </KeyboardAwareScrollView>
   );
 }
 
